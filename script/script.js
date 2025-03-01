7b9510cd701bda8521c1eb0fc6f9b4891e7e1308
Guilherme Bonifácio <109730367+Goneleven@users.noreply.github.com>
2025-03-01 13:24:32 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,16 @@
+// var acessoPermitido
+// var idade = prompt("Digite sua idade");
+ 
+// if(idade >= 18){
+//     acessoPermitido = true
+// }else{
+//     acessoPermitido = false
+ 
+// }
+ 
+// alert(acessoPermitido? "Acesso Permitido": "Acesso Negado");
+ 
+// if(acessoPermitido){
+//     var nome = prompt("Qual é seu nome em?");
+//     alert("Você pode olhar o site senhor "+ nome)
+// }
